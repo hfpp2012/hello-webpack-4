180eff1d703286b105b00a4a573ebecac3a360fb
hfpp2012 <hfpp2012@gmail.com>
2018-10-24 09:51:49 +0800

    15_babel/polyfill_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import person from './lib';
+// async function getPosts() {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await response.json();
+//   return data;
+// }
+//
+// getPosts().then(posts => console.log(posts))
 
-console.log(person);
+Object.assign({})
+
+Array.from([1,2,3])
+
+new Promise(resolve => console.log('promise'))
