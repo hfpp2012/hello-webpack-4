883792098ccdc2deafd13732c46a304c7e73961b
hfpp2012 <hfpp2012@gmail.com>
2018-12-13 10:46:14 +0800

    31_webpackbar

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackBar = require('webpackbar');
 
 let pathsToClean = [
   'dist'
@@ -63,6 +64,7 @@ module.exports = {
       filename: 'index.html',
       template: 'public/index.html',
       chunks: ["app"]
-    })
+    }),
+    new WebpackBar()
   ]
 }
